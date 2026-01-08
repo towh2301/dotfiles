f1cb0edc06cd9d2f3f2bce28cb8676cef6e478a0
towh2301 <huy.buihoang2301@gmail.com>
2026-01-08 09:23:34 +0700

    update

--- .config/waybar/scripts/Cava.sh
+++ .config/waybar/scripts/Cava.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-is_cava_ServerExist=`ps -ef|grep -m 1 cava|grep -v "grep"|wc -l`
+is_cava_ServerExist=$(ps -ef | grep -m 1 cava | grep -v "grep" | wc -l)
 if [ "$is_cava_ServerExist" = "0" ]; then
-	echo "cava_server not found" > /dev/null 2>&1
+  echo "cava_server not found" >/dev/null 2>&1
 #	exit;
 elif [ "$is_cava_ServerExist" = "1" ]; then
   killall cava
